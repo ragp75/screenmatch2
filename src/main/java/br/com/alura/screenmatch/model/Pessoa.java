6d8a64cb371a8db559ae7047f9284fa89ca6aba4
ragp75 <renato.pinheiro75@gmail.com>
2026-04-07 12:45:35 -0300

    committing with exercises from chapter 3

---
+++ src/main/java/br/com/alura/screenmatch/model/Pessoa.java
@@ -0,0 +1,27 @@
+package br.com.alura.screenmatch.model;
+
+public class Pessoa {
+    String nome;
+    int idade;
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                '}';
+    }
+}
